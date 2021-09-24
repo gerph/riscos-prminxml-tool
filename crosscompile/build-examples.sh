c8af0b414d4715ca9219b3027a04c414ceedc5d6
Charles Ferguson <gerph@gerph.org>
2021-09-24 18:40:19 +0100

    Add warning when the XML_CATALOG_FILES for our resource do not exist.
    Alan's seeing an odd problem where the system is failing to set the
    XML_CATALOG_FILES environment variable. The only way that should happen
    is if the catalog files don't exist.
    
    This change introduces a warning that is displayed when the problem
    happens.
    
    Debian has been added to the platforms we test.

--- crosscompile/build-examples.sh
+++ crosscompile/build-examples.sh
@@ -22,6 +22,7 @@
 #   macOS
 #   Ubuntu Linux (18.04-21.04)
 #   Centos (7 and 8)
+#   Debian (10)
 #
 
 set -e
@@ -35,7 +36,7 @@ if [[ "$SYSTEM" = 'Darwin' ]] ; then
     DISTRO_RELEASE='unknown'
 else
     if [[ -f '/etc/lsb-release' ]] ; then
-        # Probably an Ubuntu.
+        # Probably an Ubuntu or Debian
         DISTRO=$(source /etc/lsb-release ; echo $DISTRIB_ID | tr A-Z a-z)
         DISTRO_RELEASE=$(source /etc/lsb-release ; echo $DISTRIB_RELEASE)
     elif [[ -f '/etc/os-release' ]] ; then
@@ -181,6 +182,8 @@ if ! type -p prince >/dev/null 2>&1 && [[ "$PRINCEXML_I_HAVE_A_LICENSE" = 1 ]] ;
                 fi
                 # FIXME: Determine the actual architecture
                 PRINCE_ARCH='amd64'
+            elif [[ "$DISTRO" = 'debian' ]] ; then
+                PRINCE_ARCH='amd64'
             elif [[ "$DISTRO" = 'centos' ]] ; then
                 PRINCE_ARCH='x86_64'
             fi
@@ -222,8 +225,6 @@ if ! type -p prince >/dev/null 2>&1 && [[ "$PRINCEXML_I_HAVE_A_LICENSE" = 1 ]] ;
 
             if [[ "$SYSTEM" = 'Linux' ]] ; then
                 # We also seem to need some libraries to be installed (there's no binary so this will just install)
-                :
-                bash
                 install_package libtiff5
                 install_package libgif7
                 install_package libpng16-16
